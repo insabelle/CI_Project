ec48748737d1bd1b6d0575fb2cca32a4b74c2382
Insaf Bellamine <insaf@email.com>
2026-06-07 03:16:23 +0100

    Initial Commit

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import *
+
+def test_addition():
+    assert addition(4, 2) == 6
+
+def test_soustraction():
+    assert soustraction(5, 2) == 3
+
+def test_multiplication():
+    assert multiplication(3, 2) == 6
+
+def test_division():
+    assert division(8, 2) == 4
